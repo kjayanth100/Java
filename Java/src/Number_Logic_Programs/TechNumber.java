85105f74f8c060256346c60263d85887293a9344
kjayanth100 <kjayanthbablu100@gmail.com>
2025-12-06 08:32:32 +0530

    Number_Logic

---
+++ Java/src/Number_Logic_Programs/TechNumber.java
@@ -0,0 +1,27 @@
+package Number_Logic_Programs;
+
+public class TechNumber {
+	public static boolean isTech(int num) {
+		int count=0,temp=num,div=1;
+		while(num!=0) {
+			count++;
+			num/=10;
+		}
+		for(int i=1;i<=count/2;i++) {
+			div*=10;
+		}
+		int left=temp/div;
+		int right=temp%div;
+		int sum=left+right;
+		sum*=sum;
+		return sum==temp;
+		}
+	public static void main(String[] args) {
+		if(isTech(2025)) {
+			System.out.println("Tech");
+		}else {
+			System.out.println("!Tech");
+		}
+	}
+
+}
